ab638e7d5c156145b36cbc241958e693dd62da3e
llt <418797644@qq.com>
2023-08-06 21:58:08 +0800

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,20 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import AuthButtonClient from './auth-button-client'
+import { redirect } from 'next/navigation'
+import AuthButtonClient from '../auth-button-client'
 
-async function AuthButtonServer() {
+async function Page() {
   const supabase = createServerComponentClient<Database>({ cookies })
+
   const {
     data: { session },
   } = await supabase.auth.getSession()
+
+  if (session) {
+    redirect('/')
+  }
+
   return <AuthButtonClient session={session} />
 }
 
-export default AuthButtonServer
+export default Page
