6bcc0709283a6622b5a3371fa880f12036dcf622
llt <418797644@qq.com>
2023-08-13 16:58:03 +0800

    update

--- src/app/auth-button-client.tsx
+++ src/app/auth-button-client.tsx
@@ -14,7 +14,7 @@ function AuthButtonClient({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: 'http://localhost:3000/auth/callback',
+        redirectTo: `${location.origin}/auth/callback`,
       },
     })
   }
